c0b42ed2b525a7dd0d6f724303a80a8b26feeaed
Bayu Erich <rich.bayue@gmail.com>
2026-07-16 14:48:41 +0700

    fix(gui): resolve mypy error by using getattr for _MEIPASS

--- gui/gui.py
+++ gui/gui.py
@@ -83,7 +83,7 @@ GooeyApplication.onClose = patched_on_close
 
 def get_resource_path(relative_path):
     if getattr(sys, "frozen", False):
-        return os.path.join(sys._MEIPASS, "gui", relative_path)
+        return os.path.join(getattr(sys, "_MEIPASS"), "gui", relative_path)
     return os.path.join(os.path.dirname(__file__), relative_path)
 
 
